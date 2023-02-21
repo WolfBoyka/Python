1898bcb129f4b7d788e813b8c13e5a4422550146
Wolf Boyka <ocamus@hotmail.fr>
2023-02-21 23:49:20 +0100

    Script test code retour complet

---
+++ Alerte.py
@@ -0,0 +1,3 @@
+class Alerte:
+    def getAlerte(self, response):
+        return response.status_code
--- script_check_html_code.py
+++ script_check_html_code.py
@@ -1,10 +1,46 @@
 import requests
+import datetime
+import sys
+
+from Alerte import Alerte
 
 # script de check de code html
-print("Check html code for google.com"
-)
+siteToRequest = input("Entrez le site que vous souhaitez tester : ")
+urlCompleted = "http://" + siteToRequest
+print("Vous souhaitez tester le site : " + urlCompleted)
 
-url = "https://www.google.com"
-response = requests.get(url)
+try:
+    
+    urlRedirect = requests.get(urlCompleted, allow_redirects=True)
+    if urlRedirect.history:
+        print("La requete a ete redirigee.")
+        for r in urlRedirect.history:
+            print("Code retour : " + str(r.status_code))
+        print("La nouvelle destination est :", urlRedirect.url)
+        urlToVerify = requests.head(str(urlRedirect.url))
+    else:
+        print("La requete n'a pas ete redirigee.")
+        urlToVerify = requests.head(urlCompleted)
 
-print("Code retour HTTP :", response.status_code)
+except Exception as error:
+    print("Erreur lors de la verification de : " + urlCompleted + " => ", error)
+    print("L'url donnee est invalide !")
+    sys.exit(1)
+
+print("Teste du site : " + urlToVerify.url)
+
+maintenant = datetime.datetime.now()
+
+with open("Script_html_check_code_results\\result.txt", 'a') as f:
+    f.write("\n" + str(maintenant) + " | Code retour du site : " + urlToVerify.url + " : " + str(urlToVerify.status_code))
+
+alerte = Alerte()
+if alerte.getAlerte(urlToVerify) == 200:
+
+    urlVerified = requests.get(urlToVerify.url)
+    with open("Script_html_check_code_results\\result.txt", 'a') as f:
+        f.write("\nCode retour OK")
+
+else : 
+    with open("Script_html_check_code_results\\result.txt", 'a') as f:
+        f.write("\nCode retour KO")
